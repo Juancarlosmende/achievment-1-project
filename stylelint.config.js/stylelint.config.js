609c1a3839bd24d0bd39881b6465879eeb0ef940
Juancarlosmende <128351394+Juancarlosmende@users.noreply.github.com>
2023-05-08 12:48:20 -0500

    Initial commit

---
+++ stylelint.config.js/stylelint.config.js
@@ -0,0 +1,6 @@
+Name: Stylelint
+Id: stylelint.vscode-stylelint
+Description: Official Stylelint extension for Visual Studio Code
+Version: 1.2.4
+Publisher: Stylelint
+VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=stylelint.vscode-stylelint
